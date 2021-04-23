6e0c60085ebfcef9f22835f7b361bffb1d9eef00
Alexandr Volkov <al.volkov@qsoft.ru>
2021-04-23 18:49:06 +0300

    Навигация через меню

---
+++ app/src/main/java/com/example/weatherapp/HistoryFragment.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.weatherapp.databinding.FragmentHistoryBinding
+
+
+class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history, container, false)
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/weatherapp/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/WeatherFragment.kt
@@ -2,14 +2,13 @@ package com.example.weatherapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.weatherapp.data.Weather
 import com.example.weatherapp.databinding.FragmentWeatherBinding
@@ -22,6 +21,11 @@ class WeatherFragment : Fragment() {
     private lateinit var binding: FragmentWeatherBinding
     private lateinit var apiViewModel: WeatherApiViewModel
     private lateinit var dbViewModel: WeatherDatabaseViewModel
+
+    companion object {
+        private const val DEFAULT_CITY = "Moscow"
+        private const val DATE_FORMAT = "yyyy/MM/dd HH:mm"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -81,8 +85,21 @@ class WeatherFragment : Fragment() {
         }
     }
 
-    companion object {
-        private const val DEFAULT_CITY = "Moscow"
-        private const val DATE_FORMAT = "yyyy/MM/dd HH:mm"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.historyMenuItem -> this.findNavController().navigate(R.id.action_weatherFragment_to_historyFragment)
+        }
+
+        return true
     }
 }
