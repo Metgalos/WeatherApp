7848e55d13b3500f7aadeb4be01875213b09f456
Alexandr Volkov <al.volkov@qsoft.ru>
2021-04-30 13:00:29 +0300

    Использование корутины с использованием Dispatchers для оптимизации производительности

--- app/src/main/java/com/example/weatherapp/weatherapi/WeatherApiViewModel.kt
+++ app/src/main/java/com/example/weatherapp/weatherapi/WeatherApiViewModel.kt
@@ -7,6 +7,7 @@ import com.example.weatherapp.App
 import com.example.weatherapp.fragments.search.WeatherFragmentNavigation
 import com.example.weatherapp.infrastructure.viewmodel.Emitter
 import com.example.weatherapp.weatherapi.models.Weather
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
@@ -18,7 +19,7 @@ class WeatherApiViewModel (
     val myResponse: MutableLiveData<Response<Weather>> = MutableLiveData()
 
     fun doOnGetCurrentWeatherButtonClick(city: String) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.Default) {
             myResponse.value = weatherApi.getCurrentWeather(city)
             emitter.emitAndExecute(WeatherFragmentNavigation.GetCurrentWeather())
         }
