411065a25f1477eb2db73664f9df399e05d1f7b1
Alexandr Volkov <al.volkov@qsoft.ru>
2021-05-12 17:34:01 +0300

    Исправлена функция-расширение для преобразования дат

--- app/src/main/java/com/example/weatherapp/extensions/DateExtensions.kt
+++ app/src/main/java/com/example/weatherapp/extensions/DateExtensions.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.extensions
 
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -8,12 +9,12 @@ fun String.toUiDateFormat(): String {
     return changeDateFormat(this, "yyyy/MM/dd HH:mm:ss", "dd MMMM yyyy HH:mm")
 }
 
-fun String.toDbDateFormat(): String {
-    return changeDateFormat(this, "dd MMMM yyyy HH:mm", "yyyy/MM/dd HH:mm:ss")
-}
-
 private fun changeDateFormat(string: String, from: String, to: String): String {
-    val parser = SimpleDateFormat(from, Locale.US)
-    val formatter = SimpleDateFormat(to, Locale.US)
-    return formatter.format(parser.parse(string)!!)
+    return try {
+        val parser = SimpleDateFormat(from, Locale.US)
+        val formatter = SimpleDateFormat(to, Locale.US)
+        formatter.format(parser.parse(string)!!)
+    } catch (e: ParseException) {
+        string
+    }
 }
